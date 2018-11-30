857a78c7bbb446d5679879554cce802094ef6a12
Ed Reel <edreel@gmail.com>
2018-11-29 19:00:31 -0600

    Add mozilla_firefox and update firefox package

--- packages/mozilla_firefox.rb
+++ packages/mozilla_firefox.rb
@@ -1,11 +1,11 @@
 require 'package'
 
-class Firefox < Package
+class Mozilla_firefox < Package
   description 'Mozilla Firefox (or simply Firefox) is a free and open-source web browser'
   homepage 'https://www.mozilla.org/en-US/firefox/'
-  version '63.0.3'
-  source_url 'https://ftp.mozilla.org/pub/firefox/releases/63.0.3/source/firefox-63.0.3.source.tar.xz'
-  source_sha256 '48a834daa9c5773272a30933936ea8a78b217494355749ee68996639451d0931'
+  version '61.0.2'
+  source_url 'https://www.mozilla.org/en-US/firefox/61.0.2/releasenotes/index.html'
+  source_sha256 '7db7dd912d9c8c3e48aec0cb20b53ec6b79d448d9a0b1d1e48dea569c8555c01'
 
   binary_url ({
     aarch64: '',
@@ -20,33 +20,25 @@ class Firefox < Package
      x86_64: '3a729ddcb1e0f5d63933177a35177ac6172f12edbf9fbbbf45305f49333608de',
   })
 
-  depends_on 'bz2'
-  depends_on 'libevent'
-  depends_on 'libjpeg'
-  depends_on 'libpng'
+  depends_on 'mercurial' => :build
+  depends_on 'python27' => :build
 
   def self.build
     # For detailed build instructions, see https://developer.mozilla.org/en-US/docs/Mozilla/Developer_guide/Build_Instructions/Simple_Firefox_build/Linux_and_MacOS_build_preparation.
-    Dir.chdir 'build' do
-    system '../configure',
-           "--prefix=#{CREW_PREFIX}",
-           "--libdir=#{CREW_LIB_PREFIX}",
-           '--with-system-bz2',
-           '--with-system-icu',
-           '--with-system-jpeg',
-           '--with-system-libevent',
-           #'--with-system-libvpx',
-           #'--with-system-nspr',
-           #'--with-system-nss',
-           '--with-system-png',
-           '--with-system-zlib'
-    system 'make'
+    #FileUtils.mkdir 'src'
+    #Dir.chdir 'src' do
+    #  system 'wget https://hg.mozilla.org/mozilla-central/raw-file/default/python/mozboot/bin/bootstrap.py'
+    #  abort 'Checksum mismatch. :/ Try again.'.lightred unless Digest::SHA256.hexdigest( File.read('bootstrap.py') ) == '93d8cb10d31e6f6faaca1ae76b2c0d77f0d6d7398cd5bb7cb12ecda4b78bd695'
+    #end
+    system 'hg clone https://hg.mozilla.org/mozilla-central'
+    Dir.chdir "mozilla-central" do
+      system './mach build'
     end
   end
 
   def self.install
-    Dir.chdir 'build' do
-      system "make", "DESTDIR=#{CREW_DEST_DIR}", "install"
-    end
+    #Dir.chdir 'src' do
+    #  system "python bootstrap.py"
+    #end
   end
 end
