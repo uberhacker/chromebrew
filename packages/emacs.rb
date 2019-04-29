a07e558664e9fdf99746a36bd19c6946319a2674
Ed Reel <edreel@gmail.com>
2019-04-29 00:43:58 -0500

    Add pre-built binaries for emacs and gemacs

--- packages/emacs.rb
+++ packages/emacs.rb
@@ -8,10 +8,16 @@ class Emacs < Package
   source_sha256 '151ce69dbe5b809d4492ffae4a4b153b2778459de6deb26f35691e1281a9c58e'
 
   binary_url ({
-    
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/emacs-26.2-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/emacs-26.2-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/emacs-26.2-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/emacs-26.2-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    
+    aarch64: 'f154394acc72fa516e63b3066a62438ebb43988957948d2c321003dbed68504a',
+     armv7l: 'f154394acc72fa516e63b3066a62438ebb43988957948d2c321003dbed68504a',
+       i686: 'ba2b4f1e01fa71e156fbdea69c7ff90f8267e3759cac364ec24f03c8fa19eb23',
+     x86_64: 'fbc5a5f2d5ad899cba0c85171ad03b74175265c8b344b9a8bf1002c2b5d0818b',
   })
 
   depends_on 'lcms'
--- packages/gemacs.rb
+++ packages/gemacs.rb
@@ -8,10 +8,16 @@ class Gemacs < Package
   source_sha256 '151ce69dbe5b809d4492ffae4a4b153b2778459de6deb26f35691e1281a9c58e'
 
   binary_url ({
-
+    aarch64: 'https://dl.bintray.com/chromebrew/chromebrew/gemacs-26.2-chromeos-armv7l.tar.xz',
+     armv7l: 'https://dl.bintray.com/chromebrew/chromebrew/gemacs-26.2-chromeos-armv7l.tar.xz',
+       i686: 'https://dl.bintray.com/chromebrew/chromebrew/gemacs-26.2-chromeos-i686.tar.xz',
+     x86_64: 'https://dl.bintray.com/chromebrew/chromebrew/gemacs-26.2-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-
+    aarch64: '09babc8080a5d96bd6806698e8c2c787dfc07c58f811f62dc1a152ec1be03441',
+     armv7l: '09babc8080a5d96bd6806698e8c2c787dfc07c58f811f62dc1a152ec1be03441',
+       i686: '41b850f2c9ad6d3c7e9829a7f0d252b5840c6410ae976fd2f83ef70d2af5c104',
+     x86_64: '0b22d53eb7071137632a0e01ab036b7eafd229ee323fd0c9302cadf15b3e1b81',
   })
 
   depends_on 'emacs'
