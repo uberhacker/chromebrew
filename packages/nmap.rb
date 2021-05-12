e135649739788321534ac54669a98d2c3e2d49c4
Ed Reel <edreel@gmail.com>
2021-05-11 21:07:10 -0500

    Update ncat, nmap and nping to 7.91

--- packages/nmap.rb
+++ packages/nmap.rb
@@ -1,30 +1,25 @@
 require 'package'
 
 class Nmap < Package
-  description 'Nmap (\'Network Mapper\') is a free and open source (license) utility for network discovery and security auditing.'
+  description "Nmap ('Network Mapper') is a free and open source (license) utility for network discovery and security auditing."
   homepage 'https://nmap.org/'
-  version '7.80'
+  version '7.91'
   license 'NPSL'
   compatibility 'all'
-  source_url 'https://nmap.org/dist/nmap-7.80.tar.bz2'
-  source_sha256 'fcfa5a0e42099e12e4bf7a68ebe6fde05553383a682e816a7ec9256ab4773faa'
+  source_url 'https://nmap.org/dist/nmap-7.91.tar.bz2'
+  source_sha256 '18cc4b5070511c51eb243cdd2b0b30ff9b2c4dc4544c6312f75ce3a67a593300'
 
   binary_url ({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/nmap/7.80_armv7l/nmap-7.80-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/nmap/7.80_armv7l/nmap-7.80-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/nmap/7.80_i686/nmap-7.80-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/nmap/7.80_x86_64/nmap-7.80-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '05f6f7b8303f7249e0ce26650f13bd051d0d5665d321be0ff59f6ce51cff57fe',
-     armv7l: '05f6f7b8303f7249e0ce26650f13bd051d0d5665d321be0ff59f6ce51cff57fe',
-       i686: 'f40786b4937b7d3a6b6ddc67afb4a004ba577f865d9d922fa0982ebb871b1451',
-     x86_64: '624e3f64733244d3f987b5dfbd72a52acef314fa481857cc1ad4f92d621d2f3a',
   })
 
-  def self.build
+  def self.patch
     #fixup "/usr/bin/file" -> "#{CREW_PREFIX}/bin/file" in the configure scripts
     system "sed -i s#/usr/bin/file##{CREW_DEST_PREFIX}/bin/file#g libdnet-stripped/configure"
+  end
+
+  def self.build
     system "./configure --with-pcap=linux --without-zenmap --prefix=#{CREW_PREFIX}"
     system 'make'
   end
