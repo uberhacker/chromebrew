c2461fbb3c98749b5b8eb1993ed2a8419158e886
Ed Reel <edreel@gmail.com>
2021-05-11 22:33:40 -0500

    Update libpcap from 1.9.1 to 1.10.0

--- packages/libpcap.rb
+++ packages/libpcap.rb
@@ -3,29 +3,21 @@ require 'package'
 class Libpcap < Package
   description 'A portable C/C++ library for network traffic capture.'
   homepage 'https://www.tcpdump.org/'
-  version '1.9.1'
+  version '1.10.0'
   license 'BSD'
   compatibility 'all'
-  source_url 'https://www.tcpdump.org/release/libpcap-1.9.1.tar.gz'
-  source_sha256 '635237637c5b619bcceba91900666b64d56ecb7be63f298f601ec786ce087094'
+  source_url 'https://www.tcpdump.org/release/libpcap-1.10.0.tar.gz'
+  source_sha256 '8d12b42623eeefee872f123bd0dc85d535b00df4d42e865f993c40f7bfc92b1e'
 
   binary_url ({
-    aarch64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libpcap/1.9.1_armv7l/libpcap-1.9.1-chromeos-armv7l.tar.xz',
-     armv7l: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libpcap/1.9.1_armv7l/libpcap-1.9.1-chromeos-armv7l.tar.xz',
-       i686: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libpcap/1.9.1_i686/libpcap-1.9.1-chromeos-i686.tar.xz',
-     x86_64: 'https://gitlab.com/api/v4/projects/26210301/packages/generic/libpcap/1.9.1_x86_64/libpcap-1.9.1-chromeos-x86_64.tar.xz',
   })
   binary_sha256 ({
-    aarch64: '6ed603b0fcfc2262772ab55efaf2ac4aca927131d67e796d9d7b5ebdb4a13646',
-     armv7l: '6ed603b0fcfc2262772ab55efaf2ac4aca927131d67e796d9d7b5ebdb4a13646',
-       i686: '9eac0cd730345403835a085f5acd9b1648a2610c4cf1f2ae2c3af988d4c28499',
-     x86_64: '475bcac8bcd6f24765c68151500470141d5faf85fa89879612d4a0336a43e0ef',
   })
 
   depends_on 'libnl3'
 
   def self.build
-    system "CFLAGS='-I#{CREW_PREFIX}/include/libnl3' ./configure --prefix=#{CREW_PREFIX} --libdir=#{CREW_LIB_PREFIX}"
+    system "CFLAGS='-I#{CREW_PREFIX}/include/libnl3' ./configure #{CREW_OPTIONS}"
     system 'make'
   end
 
