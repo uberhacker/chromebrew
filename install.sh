dbd30664968042bf5b0e33ec226c957f23ef2350
Ed Reel <edreel@gmail.com>
2021-05-13 00:02:25 -0500

    Add install.sh improvements

--- install.sh
+++ install.sh
@@ -16,14 +16,6 @@ CREW_DEST_DIR="${CREW_BREW_DIR}/dest"
 CREW_PACKAGES_PATH="${CREW_LIB_PATH}/packages"
 CURL="${CURL:-curl}"
 
-# EARLY_PACKAGES cannot depend on crew_profile_base for their core operations (completion scripts are fine)
-EARLY_PACKAGES="libarchive openssl zstd xzutils lz4 bz2 libxml2 pixz libssp brotli c_ares libcyrussasl libidn2 libmetalink libnghttp2 libpsl \
-libtirpc libunistring openldap rtmpdump zstd ncurses ca_certificates libyaml ruby libffi \
-nettle krb5 p11kit libtasn1 gnutls curl git icu4c "
-
-LATE_PACKAGES="binutils crew_profile_base less most manpages filecmd mawk readline perl pcre pcre2 \
-python27 python3 py3_pip sed lzip unzip zip"
-
 ARCH="$(uname -m)"
 
 GREEN='\e[1;32m';
@@ -72,33 +64,16 @@ for dir in "${CREW_CONFIG_PATH}/meta" "${CREW_DEST_DIR}" "${CREW_PACKAGES_PATH}"
   fi
 done
 
-# prepare url and sha256
-urls=()
-temp_url=
-sha256s=()
-temp_sha256=
-k=0
-
-case "${ARCH}" in
-"armv7l"|"aarch64")
-  if ! type "xz" > /dev/null; then
-    temp_url='https://github.com/snailium/chrome-cross/releases/download/v1.8.1/xz-5.2.3-chromeos-armv7l.tar.gz'
-    temp_sha256='4dc9f086ee7613ab0145ec0ed5ac804c80c620c92f515cb62bae8d3c508cbfe7'
-    urls[k]="$temp_url"
-    sha256s[k]="$temp_sha256"
-    k=$((k+1))
-  fi
-  ;;
-esac
-
-for package in $EARLY_PACKAGES; do
-  pkgfile="$($CURL -Lsf "${URL}"/packages/"$package".rb)"
-  temp_url="$(echo "$pkgfile" | grep -m 3 "$ARCH": | head -n 1 | cut -d\' -f2 | tr -d \' | tr -d \" | sed 's/,//g')"
-  temp_sha256="$(echo "$pkgfile" | grep -m 3 "$ARCH": | tail -n 1 | cut -d\' -f2 | tr -d \' | tr -d \" | sed 's/,//g')"
-  urls[k]="$temp_url"
-  sha256s[k]="$temp_sha256"
-  k=$((k+1))
-done
+# create the device.json file if it doesn't exist
+cd "${CREW_CONFIG_PATH}"
+if [ ! -f device.json ]; then
+  echo -e "${YELLOW}Creating new device.json...${RESET}"
+  echo '{' > device.json
+  echo '  "architecture": "'"${ARCH}"'",' >> device.json
+  echo '  "installed_packages": [' >> device.json
+  echo '  ]' >> device.json
+  echo '}' >> device.json
+fi
 
 # functions to maintain packages
 function download_check () {
@@ -129,7 +104,7 @@ function extract_install () {
 
     #extract and install
     echo "Extracting ${1} (this may take a while)..."
-    if ! LD_LIBRARY_PATH=${CREW_PREFIX}/lib${LIB_SUFFIX}:/lib${LIB_SUFFIX} pixz -h &> /dev/null; then 
+    if ! LD_LIBRARY_PATH=${CREW_PREFIX}/lib${LIB_SUFFIX}:/lib${LIB_SUFFIX} pixz -h &> /dev/null; then
       tar xpf ../"${2}"
     else
       LD_LIBRARY_PATH=${CREW_PREFIX}/lib${LIB_SUFFIX}:/lib${LIB_SUFFIX} tar -Ipixz -xpf ../"${2}"
@@ -163,31 +138,56 @@ function update_device_json () {
   fi
 }
 
-# create the device.json file if it doesn't exist
-cd "${CREW_CONFIG_PATH}"
-if [ ! -f device.json ]; then
-  echo -e "${YELLOW}Creating new device.json...${RESET}"
-  echo '{' > device.json
-  echo '  "architecture": "'"${ARCH}"'",' >> device.json
-  echo '  "installed_packages": [' >> device.json
-  echo '  ]' >> device.json
-  echo '}' >> device.json
-fi
+function download_extract_update () {
+  for package in ${1}; do
+    pkgfile="$($CURL -Lsf "${URL}"/packages/"$package".rb)"
+    url="$(echo "$pkgfile" | grep -m 3 "$ARCH": | head -n 1 | cut -d\' -f2 | tr -d \' | tr -d \" | sed 's/,//g')"
+    sha256="$(echo "$pkgfile" | grep -m 3 "$ARCH": | tail -n 1 | cut -d\' -f2 | tr -d \' | tr -d \" | sed 's/,//g')"
+    tarfile="$(basename ${url})"
+    name="${tarfile%%-*}"   # extract string before first '-'
+    rest="${tarfile#*-}"    # extract string after first '-'
+    version="$(echo ${rest} | sed -e 's/-chromeos.*$//')" # extract string between first '-' and "-chromeos"
+    download_check "${name}" "${url}" "${tarfile}" "${sha256}"
+    extract_install "${name}" "${tarfile}"
+    update_device_json "${name}" "${version}"
+  done
+}
 
-# extract, install and register packages
-for i in $(seq 0 $((${#urls[@]} - 1))); do
-  url="${urls["${i}"]}"
-  sha256="${sha256s["${i}"]}"
-  tarfile="$(basename ${url})"
-  name="${tarfile%%-*}"   # extract string before first '-'
-  rest="${tarfile#*-}"    # extract string after first '-'
-  version="$(echo ${rest} | sed -e 's/-chromeos.*$//')"
-                        # extract string between first '-' and "-chromeos"
+case "${ARCH}" in
+"armv7l"|"aarch64")
+  if ! type "xz" > /dev/null; then
+    url='https://github.com/snailium/chrome-cross/releases/download/v1.8.1/xz-5.2.3-chromeos-armv7l.tar.gz'
+    sha256='4dc9f086ee7613ab0145ec0ed5ac804c80c620c92f515cb62bae8d3c508cbfe7'
+    tarfile="$(basename ${url})"
+    name="${tarfile%%-*}"   # extract string before first '-'
+    rest="${tarfile#*-}"    # extract string after first '-'
+    version="$(echo ${rest} | sed -e 's/-chromeos.*$//')" # extract string between first '-' and "-chromeos"
+    download_check "${name}" "${url}" "${tarfile}" "${sha256}"
+    extract_install "${name}" "${tarfile}"
+    update_device_json "${name}" "${version}"
+  fi
+  ;;
+esac
 
-  download_check "${name}" "${url}" "${tarfile}" "${sha256}"
-  extract_install "${name}" "${tarfile}"
-  update_device_json "${name}" "${version}"
+# install packages to get pixz working
+# NOTE: pixz dependency package binaries cannot use pixz
+COMPRESSION_PACKAGES="libarchive openssl zstd xzutils lz4 bz2 libxml2 pixz"
+download_extract_update "${COMPRESSION_PACKAGES}"
+
+# install remaining core packages
+# NOTE: sha256 needs updated if core_packages.txt is changed
+name=core_packages.txt
+tmpfile=/tmp/${name}
+url=https://raw.githubusercontent.com/${OWNER}/${REPO}/master/tools/${name}
+sha256=d0fa5af6098ab037aa1952af3790874a2a25f5899419fabbc3530b895a46ba20
+download_check "${name}" "${url}" "${tmpfile}" "${sha256}"
+CORE_PACKAGES=$(cat "${tmpfile}" | xargs)
+rm -f "${tmpfile}"
+PACKAGES=
+for package in ${CORE_PACKAGES}; do
+  [ -z "$(echo ${COMPRESSION_PACKAGES} | tr ' ' '\n' | grep -e ^${package}$)" ] && PACKAGES+=" $package"
 done
+download_extract_update "${PACKAGES}"
 
 ## workaround https://github.com/skycocker/chromebrew/issues/3305
 sudo ldconfig &> /dev/null || true
@@ -209,8 +209,6 @@ echo tools   >> .git/info/sparse-checkout
 git fetch origin "${BRANCH}"
 git reset --hard origin/"${BRANCH}"
 crew update
-
-yes | crew install $LATE_PACKAGES 
 
 [[ "$devtools" == "y" || "$devtools" == "Y" ]] && yes | crew install buildessential
 
