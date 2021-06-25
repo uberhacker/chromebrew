41bba8a5f92d4ee365d9efbf95e17b42e9937d18
Ed Reel <edreel@gmail.com>
2021-06-25 01:39:37 -0500

    Add ccache dependency

--- packages/zig.rb
+++ packages/zig.rb
@@ -9,16 +9,19 @@ class Zig < Package
   source_url 'https://ziglang.org/download/0.8.0/zig-0.8.0.tar.xz'
   source_sha256 '03a828d00c06b2e3bb8b7ff706997fd76bf32503b08d759756155b6e8c981e77'
 
- def self.build
-   Dir.mkdir 'build'
-   Dir.chdir 'build' do
-     system "cmake #{CREW_CMAKE_OPTIONS} .."
-     system 'make'
-   end
- end
- def self.install
-   Dir.chdir 'build' do
-     system "DESTDIR=#{CREW_DEST_DIR} make install"
-   end
+  depends_on 'ccache' => :build
+
+  def self.build
+    Dir.mkdir 'build'
+    Dir.chdir 'build' do
+      system "cmake -DCMAKE_INSTALL_PREFIX=#{CREW_PREFIX} -DZIG_USE_CCACHE=ON -DCMAKE_BUILD_TYPE=Release .."
+      system 'make'
+    end
+  end
+
+  def self.install
+    Dir.chdir 'build' do
+      system "DESTDIR=#{CREW_DEST_DIR} make install"
+    end
   end
 end
